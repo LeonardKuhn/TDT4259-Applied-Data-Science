877e15c5edde77c8ef027d4d45dbaa07c5b6a4e6
Marcus Stephan Nordal <marcusn1301@gmail.com>
2023-11-22 15:08:16 +0100

    compare to actual values

--- decision_tree_regressor.py
+++ decision_tree_regressor.py
@@ -8,9 +8,9 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 data = pd.read_csv("./consumption_temp.csv")
 
-print(data.describe())
-print(data.head())
-print(data.tail())
+# print(data.describe())
+# print(data.head())
+# print(data.tail())
 
 # Data preprocessing
 data['time'] = pd.to_datetime(data['time'])
@@ -30,7 +30,7 @@ y = data['consumption']
 X = data[['year', 'month', 'day', 'hour', 'day_of_week', 'temperature', 'Location_bergen', 'Location_oslo', 'Location_stavanger', 'Location_tromsø', 'Location_trondheim']]
 
 # Split data: Training and testing sets (80% training, 20% testing)
-forecast_date = datetime(2023, 7, 17)
+forecast_date = datetime(2023, 3, 17)
 # Historical data limitation: 5 days
 historical_data_end_date = forecast_date - timedelta(days=5)
 train_data = data[data['time'] <= historical_data_end_date]
@@ -44,7 +44,7 @@ aneo_model.fit(X_train, y_train)
 
 # Predict consumption values for the specified date and time
 start_date = forecast_date
-end_date = datetime(2023, 7, 18)
+end_date = datetime(2023, 3, 18)
 date_range = [start_date + timedelta(hours=i) for i in range((end_date - start_date).days * 24)]
 
 predicted_consumption = []
@@ -61,10 +61,10 @@ for date in date_range:
         'day_of_week': [date.weekday()],
         'temperature': [temperatureLastFiveDays],  
         # Choose the location by having 1 for the location and 0 for the rest
-        'Location_bergen': [0],
+        'Location_bergen': [1],
         'Location_oslo': [0],
         'Location_stavanger': [0],
-        'Location_tromsø': [1],
+        'Location_tromsø': [0],
         'Location_trondheim': [0]   
     }
 
@@ -81,6 +81,16 @@ print(f'Mean Absolute Error on the test data: {mae:.2f}')
 print(f'Mean Squared Error on the test data: {mse:.2f}')
 print(f'R^2 Score on the test data: {r2:.2f}')
 
+actual_consumption = []
+
+for date in date_range:
+    actual_consumption_val = data[data['time'] == date]['consumption'].values
+    if len(actual_consumption_val) > 0:
+        actual_consumption.append(actual_consumption_val[0])
+    else:
+        # Handle missing values if any
+        actual_consumption.append(np.nan)
+
 def datetime_to_str(date):
     return date.strftime('%Y/%m/%d')
 
@@ -89,14 +99,16 @@ time_labels = [date.strftime('%H:%M') for date in date_range]
 
 # Predicted consumption values
 plt.figure(figsize=(14, 5))
-plt.plot(date_labels, predicted_consumption, marker=',', linestyle='-')
-plt.title(f'''Tromsø: {datetime_to_str(start_date)} - {datetime_to_str(end_date)}''')
+plt.plot(date_labels, predicted_consumption, marker='v', linestyle='-', label='Predicted')
+plt.plot(date_labels, actual_consumption, marker='o', linestyle='-', label='Actual', color='orange')
+plt.title(f'''Bergen: {datetime_to_str(start_date)} - {datetime_to_str(end_date)}''')
 plt.ylabel('Consumption (MW)')
+plt.legend()
 plt.grid(False)
 plt.xticks(range(len(date_range)), time_labels, rotation=45)
 
-metrics_table = f'MAE: {mae:.2f}\nMSE: {mse:.2f}\nR2: {r2:.2f}'
-plt.annotate(metrics_table, xy=(0.5, 0.85), xycoords='axes fraction', ha='left', va='top', fontsize=10, color='blue')
+# metrics_table = f'MAE: {mae:.2f}\nMSE: {mse:.2f}\nR2: {r2:.2f}'
+# plt.annotate(metrics_table, xy=(0.5, 0.85), xycoords='axes fraction', ha='left', va='top', fontsize=10, color='blue')
 
 plt.subplots_adjust(left=0.1) 
 plt.show()
